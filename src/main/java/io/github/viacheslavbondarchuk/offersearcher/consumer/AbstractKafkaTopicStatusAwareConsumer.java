37009ebc884f5f1053b0b097e5d04a5e1f87d52d
vbondarchuk <Viacheslav.Bondarchuk@onseo.biz>
2023-12-23 21:21:34 +0200

    fixes

--- src/main/java/io/github/viacheslavbondarchuk/offersearcher/consumer/AbstractKafkaTopicStatusAwareConsumer.java
+++ src/main/java/io/github/viacheslavbondarchuk/offersearcher/consumer/AbstractKafkaTopicStatusAwareConsumer.java
@@ -7,10 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.SynchronousQueue;
 
 import static java.lang.Long.max;
 
@@ -18,6 +23,7 @@ import static java.lang.Long.max;
 public abstract class AbstractKafkaTopicStatusAwareConsumer<K, V> implements KafkaConsumer<K, V>, StatusAwareService<KafkaTopicStatus> {
     protected final KeyValuePair<String, Long> topicMaxOffsetPair;
     protected final KeyValuePair<String, Long> topicCurrentOffsetPair;
+    protected final Queue<List<ConsumerRecord<K, V>>> queue;
     protected final String topic;
 
     protected AbstractKafkaTopicStatusAwareConsumer(Consumer<String, String> consumer, String topic) {
@@ -32,6 +38,7 @@ public abstract class AbstractKafkaTopicStatusAwareConsumer<K, V> implements Kaf
                 .stream()
                 .reduce(0L, Long::sum) - 1);
         this.topicCurrentOffsetPair = KeyValuePair.of(topic, 0L);
+        this.queue = new SynchronousQueue<>();
         this.topic = topic;
     }
 
@@ -51,14 +58,20 @@ public abstract class AbstractKafkaTopicStatusAwareConsumer<K, V> implements Kaf
 
     @Override
     public void onRecords(List<ConsumerRecord<K, V>> records) {
-        for (ConsumerRecord<K, V> record : records) {
-            try {
-                log.info("Processing kafka record. Key: {}, Value: {}", record.key(), record.value());
-                updateCurrentOffset(record.offset());
-                onRecord(record);
-            } catch (Exception ex) {
-                log.error("Can not process kafka record: Key: {}, Value: {}. Exception: ", record.key(), record.value(), ex);
-            }
-        }
+        queue.addAll((Collection<? extends List<ConsumerRecord<K, V>>>) records);
+    }
+
+    @Scheduled(fixedRate = 5000)
+    private void handle() {
+        Optional.ofNullable(queue.poll())
+                .ifPresent(records -> records.forEach(record -> {
+                    try {
+                        log.info("Processing kafka record. Key: {}, Value: {}", record.key(), record.value());
+                        updateCurrentOffset(record.offset());
+                        onRecord(record);
+                    } catch (Exception ex) {
+                        log.error("Can not process kafka record: Key: {}, Value: {}. Exception: ", record.key(), record.value(), ex);
+                    }
+                }));
     }
 }
